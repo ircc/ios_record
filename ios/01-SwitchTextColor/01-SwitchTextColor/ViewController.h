9f71267c9712b34535980833d3d98afef61f8a10
Ircc <happyhaoyun@qq.com>
2016-04-29 01:20:42 +0800

    UI第一个文本切换颜色代码

---
+++ ios/01-SwitchTextColor/01-SwitchTextColor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  01-SwitchTextColor
+//
+//  Created by ray on 4/28/16.
+//  Copyright © 2016 ray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ios/01-SwitchTextColor/01-SwitchTextColor/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  01-SwitchTextColor
+//
+//  Created by ray on 4/28/16.
+//  Copyright © 2016 ray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
